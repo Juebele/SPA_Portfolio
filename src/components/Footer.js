f6aafdc9471fc6273586511688d6bd0396c0911b
Juebele <eronuebele@gmail.com>
2023-06-03 15:56:32 -0400

    added images and reset links/project names

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,13 @@ function Footer(props) {
     console.log(props)
 
     return (
-        <div class="container">
+        <div class="container footer">
+        <div class="links">
             <ul>
                 <li><a href="https://github.com/Juebele" target="_blank">github</a></li>
                 <li><a href="https://www.linkedin.com/in/juebele/" target="blank">LinkedIn</a></li>
             </ul>
+        </div>
         </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,21 +7,21 @@ function Main(props) {
 <section class="container">
     <div class="card">
         <div class="card-image project-1"></div>
-        <h2>Project 1</h2>
-        <p>The Cessna 172 Skyhawk is an American four-seat, single-engine, high wing, fixed-wing aircraft made by the Cessna Aircraft Company.[2] First flown in 1955,[2] more 172s have been built than any other aircraft.</p>
-        <a href="https://en.wikipedia.org/wiki/Cessna_172" target="_blank">Learn More</a>
+        <h2>Market Research Tool</h2>
+        <p>This group project uses third-party APIs to fetch live stock market data. This was an early experience with group work that taught me how to use Git collaboratively.</p>
+        <a href="https://juebele.github.io/Ticker-Lookup/" target="_blank">Learn More</a>
     </div>
     <div class="card">
         <div class="card-image project-2"></div>
-        <h2>Project 2</h2>
-        <p>The Cessna 208 Caravan is a utility aircraft produced by Cessna. The project was commenced on November 20, 1981, and the prototype first flew on December 9, 1982.</p>
-        <a href="https://en.wikipedia.org/wiki/Cessna_208_Caravan" target="_blank">Learn More</a>
+        <h2>Flapjax Official Site</h2>
+        <p>This is a website that I built for a local catering business. It includes professional photos, info about the business, and a link to send the owner an email.</p>
+        <a href="https://juebele.github.io/Flapjax-Website/" target="_blank">Learn More</a>
     </div>
     <div class="card">
         <div class="card-image project-3"></div>
-        <h2>Project 3</h2>
-        <p>The Cessna Citation II are light corporate jets built by Cessna as part of the Citation family. Stretched from the Citation I, the Model 550 was announced in September 1976, first flew on January 31, 1977, and was certified in March 1978. </p>
-        <a href="https://en.wikipedia.org/wiki/Cessna_CitationJet/M2" target="_blank">Learn More</a>
+        <h2>Note-taking App</h2>
+        <p>This project tasked me with learning how to use Express to build a note-taking app. This was my first experience using Heroku to build a full-stack application.</p>
+        <a href="https://note-taking-app-juebele.herokuapp.com/" target="_blank">Learn More</a>
     </div>
 </section>
     )
