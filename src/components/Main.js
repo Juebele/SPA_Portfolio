28028b2060b322d55dced1de1e9d93fe2281d07c
Juebele <eronuebele@gmail.com>
2023-06-05 17:35:30 -0400

    centered some stuf

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,13 +11,13 @@ function Main(props) {
         <p>This group project uses third-party APIs to fetch live stock market data. This was an early experience with group work that taught me how to use Git collaboratively.</p>
         <a href="https://juebele.github.io/Ticker-Lookup/" target="_blank">Learn More</a>
     </div>
-    <div class="card col-5">
+    <div class="card col-10">
         <div class="card-image project-2"></div>
         <h2>Flapjax Official Site</h2>
         <p>This is a website that I built for a local catering business. It includes professional photos, info about the business, and a link to send the owner an email.</p>
         <a href="https://juebele.github.io/Flapjax-Website/" target="_blank">Learn More</a>
     </div>
-    <div class="card col-5">
+    <div class="card col-10">
         <div class="card-image project-3"></div>
         <h2>Note-taking App</h2>
         <p>This project tasked me with learning how to use Express to build a note-taking app. This was my first experience using Heroku to build a full-stack application.</p>
