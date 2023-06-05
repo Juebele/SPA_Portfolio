64be59fc3c429bb1a8d1a1de838dbe5f0e3694db
Juebele <eronuebele@gmail.com>
2023-06-05 17:45:11 -0400

    added about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ function About(props) {
 
     return (
         <div class="about">
-            <div class="headshot">
-                
-            </div>
-            <div class="about-text">blank</div>
+            <div class="headshot"></div>
+            <div class="about-text">Hello! I'm currently a Web Development Student at Georgia Tech who loves learning about new technologies. My work in the supply chain industry ignited my passion for process design, which led ultimately a fascination with web development and software engineering.</div>
+            
         </div>
+        
     );
 }
 
