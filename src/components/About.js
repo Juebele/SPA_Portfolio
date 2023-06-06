e7bcc5e99fd6b7768a422cca40c230e7c9709760
Juebele <eronuebele@gmail.com>
2023-06-06 10:00:35 -0400

    major formatting changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Headshot from '../assets/headshot.png';
 
 function About(props) {
     console.log(props)
 
     return (
-        <div class="about">
-            <div class="headshot"></div>
-            <div class="about-text">Hello! I'm currently a Web Development Student at Georgia Tech who loves learning about new technologies. My work in the supply chain industry ignited my passion for process design, which led ultimately a fascination with web development and software engineering.</div>
-            
+        <section class="container">
+        <div class="row about-row">
+            <div class="col-4"><img src={Headshot} alt="an image of me" class="headshot"></img></div>
+            <div class="col-8 about-text">Hello! I'm currently a Web Development Student at Georgia Tech who loves learning about new technologies. My work in the supply chain industry ignited my passion for process design, which led ultimately a fascination with web development and software engineering.</div>
         </div>
-        
+        </section>   
     );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,24 +5,26 @@ function Main(props) {
 
     return (
 <section class="container image-row">
-    <div class="card col-10">
+<div class="row">
+    <div class="card col">
             <div class="card-image project-1"></div>
         <h2>Market Research Tool</h2>
         <p>This group project uses third-party APIs to fetch live stock market data. This was an early experience with group work that taught me how to use Git collaboratively.</p>
         <a href="https://juebele.github.io/Ticker-Lookup/" target="_blank">Learn More</a>
     </div>
-    <div class="card col-10">
+    <div class="card col">
         <div class="card-image project-2"></div>
         <h2>Flapjax Official Site</h2>
         <p>This is a website that I built for a local catering business. It includes professional photos, info about the business, and a link to send the owner an email.</p>
         <a href="https://juebele.github.io/Flapjax-Website/" target="_blank">Learn More</a>
     </div>
-    <div class="card col-10">
+    <div class="card col">
         <div class="card-image project-3"></div>
         <h2>Note-taking App</h2>
         <p>This project tasked me with learning how to use Express to build a note-taking app. This was my first experience using Heroku to build a full-stack application.</p>
         <a href="https://note-taking-app-juebele.herokuapp.com/" target="_blank">Learn More</a>
     </div>
+</div>
 </section>
     )
 }
