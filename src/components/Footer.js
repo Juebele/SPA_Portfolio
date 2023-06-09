5c1f57a8db57e5ef27763bbac50667ec9746faf5
Juebele <eronuebele@gmail.com>
2023-06-09 13:06:09 -0400

    minor formatting changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,9 @@ function Footer(props) {
         <div class="container footer">
         <div class="links">
             <ul>
-                <li><a href="https://github.com/Juebele" target="_blank">github</a></li>
+                <li><a href="https://github.com/Juebele" target="_blank">Github</a></li>
                 <li><a href="https://www.linkedin.com/in/juebele/" target="blank">LinkedIn</a></li>
+                <li><a href="/images/myw3schoolsimage.jpg" download="Resume_2023.docx">Resume</a></li>
             </ul>
         </div>
         </div>
