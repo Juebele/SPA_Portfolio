429e65702a2975b92f20ce94173ceff3b33647fa
Juebele <eronuebele@gmail.com>
2023-06-17 12:22:55 -0400

    made more formatting changes. Need to sort out footer still

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,25 @@ function Footer(props) {
     console.log(props)
 
     return (
-        <div class="container footer">
-        <div class="links">
-            <ul>
-                <li><a href="https://github.com/Juebele" target="_blank">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/juebele/" target="blank">LinkedIn</a></li>
-                <li><a href="/images/myw3schoolsimage.jpg" download="Resume_2023.docx">Resume</a></li>
-            </ul>
-        </div>
+        <div className='container-fluid row'>
+            <div className='col'>
+            <p className='col container-fluid justify-content-end'>© 2023, James Uebele</p>
+            </div>
+            <div className='col'>
+            <p>Made with ❤️ by James Uebele</p>
+            </div>
+            <div id="contributorLinks" className='col container-fluid'>
+                <div className='row'>
+                <p><a href="https://github.com/Juebele" target="_blank" className='col footer-link'>Github</a></p>
+                <p className='col'> | </p>
+                <p><a href="https://www.linkedin.com/in/juebele/" target="blank" className='col footer-link'>LinkedIn</a></p>
+                <p className='col'> | </p>
+                <p><a href="/images/myw3schoolsimage.jpg" download="Resume_2023.docx" className='col footer-link'>Resume</a></p>
+                </div>
+            </div>
         </div>
     );
 }
 
 export default Footer
+
